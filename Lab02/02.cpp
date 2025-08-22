90b82a06d54fbababca3d26363a5a1fcc5031fcb
Takshit Mahajan <mahajantakshit635@gmail.com>
2025-08-22 09:08:50 +0530

    Update 02.cpp

--- Lab02/02.cpp
+++ Lab02/02.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
-void bubbleSort(vector<int> &arr) {
-    int n = arr.size();
-    for (int i = 0; i < n - 1; i++) {             
-        for (int j = 0; j < n - i - 1; j++) {     
-            if (arr[j] > arr[j + 1]) {            
+void bubbleSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false;
+
+        
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
                 swap(arr[j], arr[j + 1]);
+                swapped = true;
             }
         }
-    }
-    for(int val:arr){
-        cout<<val<<endl;
+
         
+        if (!swapped)
+            break;
     }
 }
-int main(){
-    vector<int> x={64,34,25,12,22,11,90};
-    bubbleSort(x);
 
+int main() {
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
 
+    cout << "Unsorted: ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted:   ";
+    for (int i = 0; i < n; i++) cout << arr[i] << " ";
+    cout << endl;
+    cout<<n;
+    return 0;
 }
-
