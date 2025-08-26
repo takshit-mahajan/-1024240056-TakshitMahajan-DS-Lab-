c16755482a0b4a788b0fcc73af8f2bdd66c88ca6
Takshit Mahajan <mahajantakshit635@gmail.com>
2025-08-26 08:45:14 +0530

    Update 01.cpp

--- Lab03/01.cpp
+++ Lab03/01.cpp
@@ -1,1 +1,56 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 5  // maximum size of stack
+
+class Stack {
+    int arr[MAX];
+    int top;
+
+public:
+    Stack() { top = -1; }
+
+    void push(int x) {
+        if (top == MAX - 1) {
+            cout << "Stack Overflow\n";
+            return;
+        }
+        arr[++top] = x;
+        cout << x << " pushed\n";
+    }
+
+    void pop() {
+        if (top == -1) {
+            cout << "Stack Underflow\n";
+            return;
+        }
+        cout << arr[top--] << " popped\n";
+    }
+
+    int peek() {
+        if (top == -1) {
+            cout << "Stack is Empty\n";
+            return -1;
+        }
+        return arr[top];
+    }
+
+    bool isEmpty() { return top == -1; }
+
+    int size() { return top + 1; }
+};
+
+int main() {
+    Stack st;
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << "Top element: " << st.peek() << endl; // 30
+    st.pop();
+    cout << "Top after pop: " << st.peek() << endl; // 20
+    cout << "Size: " << st.size() << endl;
+
+    return 0;
+}
 
